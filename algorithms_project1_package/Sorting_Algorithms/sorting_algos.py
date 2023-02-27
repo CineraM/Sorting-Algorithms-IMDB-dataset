b8cf1c354312416d6cb82250c773b74a2194a226
Matias <64340009+CineraM@users.noreply.github.com>
2023-02-27 16:02:03 -0500

    quicksort fixed

--- algorithms_project1_package/Sorting_Algorithms/sorting_algos.py
+++ algorithms_project1_package/Sorting_Algorithms/sorting_algos.py
@@ -3,7 +3,7 @@ import time
 import json
 import pandas as pd
 import sys
-
+sys.setrecursionlimit(10**6)
 """
 Note : For test cases 7-10, you need to extract the required data (filter on conditions mentioned above)
 and rename it to appropriate name as mentioned in the test case descriptions. You need to write the code
@@ -14,23 +14,24 @@ column_names= ['tconst', 'primaryTitle', 'originalTitle', 'startYear',
                'runtimeMinutes', 'genres', 'averageRating', 'numVotes', 'ordering',
                'category', 'job', 'seasonNumber', 'episodeNumber', 'primaryName', 'birthYear',
                'deathYear', 'primaryProfession']
-
-# comparison function
-# If first element is less than second return true, else false.
-def is_less(d1, d2, columns):
-    cur_idx = 0
-    while(True):
-        try:
-            if d1[columns[cur_idx]] < d2[columns[cur_idx]]:
-                return True
-            elif d1[columns[cur_idx]] == d2[columns[cur_idx]]:
-                cur_idx += 1
-            else:
-                return False
-        except:
+   
+def lessThan(d1, d2, columns):
+    for col in columns:
+        if d1[col] < d2[col]:
+            return True
+        elif d1[col] > d2[col]:
             return False
+    return False
+        
+def greaterThan(d1, d2, columns):
+    for col in columns:
+        if d1[col] > d2[col]:
+            return True
+        elif d1[col] < d2[col]:
+            return False
+    return False
 
-def is_less_or_equal(d1, d2, columns):
+def lessThanOrEqual(d1, d2, columns):
     cur_idx = 0
     while(True):
         try:
@@ -42,92 +43,29 @@ def is_less_or_equal(d1, d2, columns):
                 return False
         except:
             return True
-        
-# https://www.geeksforgeeks.org/python-program-for-quicksort/
-# Quick Sort recursive
-def pivot_element(arr, low, high, columns):
-    pivot = arr[high]
-    i = low - 1
 
-    for j in range(low, high):
-        if is_less_or_equal(arr[j], pivot, columns):
-            i = i + 1
-            # Swapping element at i with element at j
-            (arr[i], arr[j]) = (arr[j], arr[i])
- 
-    # Swap the pivot element with the greater element specified by i
-    (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])
- 
-    # Return the position from where partition is done
-    return i + 1
+def is_equal(d1, d2, columns):
+    for col in columns:
+        if d1[col] != d2[col]:
+            return False
+    return True    
 
-def quicksort_recursive(array, low, high, columns):
-    if low < high:
-        pi = pivot_element(array, low, high, columns)
- 
-        quicksort_recursive(array, low, pi - 1, columns)
- 
-        quicksort_recursive(array, pi + 1, high, columns)
-        
+# quicksort
+def pivot_element(arr):
+    #CODE For identifiying the pivot element
+    pivot = arr[len(arr)//2]
+    return pivot
 
 def quicksort(arr, columns):
-    sys.setrecursionlimit(10**6)
-    n = len(arr)
-    quicksort_recursive(arr, 0, n - 1, columns)
-    return arr
+    if len(arr) <= 1:
+        return arr
+    pivot = pivot_element(arr)
+    # rename to lessThan
+    left = [x for x in arr if lessThan(x, pivot, columns)]
+    middle = [x for x in arr if is_equal(x, pivot, columns)]
+    right = [x for x in arr if greaterThan(x, pivot, columns)]
 
-# https://www.geeksforgeeks.org/iterative-quick-sort/
-# iterative quicksort
-# def partition(arr, l, h, columns):
-#     i = ( l - 1 )
-#     x = arr[h]
-  
-#     for j in range(l, h):
-#         if is_less_or_equal(arr[j], x, columns):
-#             i = i + 1
-#             arr[i], arr[j] = arr[j], arr[i]
-  
-#     arr[i + 1], arr[h] = arr[h], arr[i + 1]
-#     return (i + 1)
-  
-# def quickSortIterative(arr, l, h, columns):
-#     size = h - l + 1
-#     stack = [0] * (size)
-  
-#     top = -1
-  
-#     top = top + 1
-#     stack[top] = l
-#     top = top + 1
-#     stack[top] = h
-  
-#     while top >= 0:
-  
-#         # Pop h and l
-#         h = stack[top]
-#         top = top - 1
-#         l = stack[top]
-#         top = top - 1
-  
-#         p = partition( arr, l, h, columns)
-  
-#         if p-1 > l:
-#             top = top + 1
-#             stack[top] = l
-#             top = top + 1
-#             stack[top] = p - 1
-  
-#         if p + 1 < h:
-#             top = top + 1
-#             stack[top] = p + 1
-#             top = top + 1
-#             stack[top] = h
-
-# def quicksort(arr, columns):
-#     n = len(arr)
-#     quickSortIterative(arr, 0, n-1, columns)
-#     return arr
-
+    return quicksort(left, columns) + middle + quicksort(right, columns)
 
 # Selection Sort
 def selection_sort(arr, columns):
@@ -137,7 +75,7 @@ def selection_sort(arr, columns):
     for i in range(0, n):
         min_idx = i
         for j in range(i+1, n):
-            if is_less(arr[j], arr[min_idx], columns):
+            if lessThan(arr[j], arr[min_idx], columns):
                 min_idx = j
             
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
@@ -152,10 +90,10 @@ def max_heapify(arr, n, i, columns):
     l = 2 * i + 1 
     r = 2 * i + 2
 
-    if l < n and is_less(arr[max], arr[l], columns):
+    if l < n and lessThan(arr[max], arr[l], columns):
         max = l
 
-    if r < n and is_less(arr[max], arr[r], columns):
+    if r < n and lessThan(arr[max], arr[r], columns):
         max = r
 
     if max != i:
@@ -192,7 +130,7 @@ def shell_sort(arr, columns):
  
             key = arr[i]
             j = i
-            while j >= gap and is_less(key, arr[j-gap], columns):
+            while j >= gap and lessThan(key, arr[j-gap], columns):
                 arr[j] = arr[j-gap]
                 j -= gap
  
@@ -206,7 +144,7 @@ def shell_sort(arr, columns):
 def merge(arr, left, right, columns):
     i=j=k=0       
     while i < len(left) and j < len(right):
-        if is_less_or_equal(left[i], right[j], columns):
+        if lessThanOrEqual(left[i], right[j], columns):
             arr[k]=left[i]
             i+=1
         else:
@@ -248,7 +186,7 @@ def insertion_sort(arr, columns):
         key = arr[i]
         j = i-1
         # key[columns[0]] < arr[j][columns[0]]
-        while j >= 0 and is_less(key, arr[j], columns):
+        while j >= 0 and lessThan(key, arr[j], columns):
                 arr[j+1] = arr[j]
                 j -= 1
         arr[j+1] = key
@@ -359,3 +297,42 @@ def sorting_algorithms(file_path, columns, select):
         end_time = time.time()
         time_in_seconds = end_time - start_time
         return [time_in_seconds, list(map(lambda x: x[0], output_list))]
+    
+# comparison function
+# If first element is less than second return true, else false.
+# def is_less(d1, d2, columns):
+#     cur_idx = 0
+#     while(True):
+#         try:
+#             if d1[columns[cur_idx]] < d2[columns[cur_idx]]:
+#                 return True
+#             elif d1[columns[cur_idx]] == d2[columns[cur_idx]]:
+#                 cur_idx += 1
+#             else:
+#                 return False
+#         except:
+#             return False
+
+# def is_equal(d1, d2, columns):
+#     cur_idx = 0
+#     while(True):
+#         try:
+#             if d1[columns[cur_idx]] == d2[columns[cur_idx]]:
+#                 cur_idx += 1
+#             else:
+#                 return False
+#         except:
+#             return True    
+        
+# def greaterThan(d1, d2, columns):
+#     cur_idx = 0
+#     while(True):
+#         try:
+#             if d1[columns[cur_idx]] < d2[columns[cur_idx]]:
+#                 return False
+#             elif d1[columns[cur_idx]] == d2[columns[cur_idx]]:
+#                 cur_idx += 1
+#             else:
+#                 return True
+#         except:
+#  
