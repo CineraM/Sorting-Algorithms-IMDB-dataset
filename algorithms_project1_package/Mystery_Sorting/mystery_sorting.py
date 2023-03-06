c2e4ac9b9945951cd9759e98473201647a9bbefc
Matias <64340009+CineraM@users.noreply.github.com>
2023-03-06 03:53:48 -0500

    meh

--- algorithms_project1_package/Mystery_Sorting/mystery_sorting.py
+++ algorithms_project1_package/Mystery_Sorting/mystery_sorting.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import shutil
 import math
 import os
 import csv
@@ -20,6 +21,22 @@ def lessThanOrEqual(d1, d2, columns):
                 return False
         except:
             return True
+        
+def lessThan(d1, d2, columns):
+    for col in columns:
+        if d1[col] < d2[col]:
+            return True
+        elif d1[col] > d2[col]:
+            return False
+    return False
+        
+def greaterThan(d1, d2, columns):
+    for col in columns:
+        if d1[col] > d2[col]:
+            return True
+        elif d1[col] < d2[col]:
+            return False
+    return False
         
 def merge(arr, left, right, columns):
     i=j=k=0       
@@ -52,6 +69,21 @@ def merge_sort(arr, columns):
         merge(arr, left, right, columns)
 
 
+def insertion_sort(arr, columns):
+    n = len(arr)
+    for i in range(1, n):
+        
+        key = arr[i]
+        j = i-1
+        
+        while j >= 0 and lessThan(key, arr[j], columns):
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
+    
+    return arr
+
+
 def get_col_indx(columns):
     list = [] 
     for i in columns: list.append(column_names.index(i))
@@ -59,59 +91,102 @@ def get_col_indx(columns):
 
 def data_chuncks(file_path, columns, memory_limitation):
         
-        column_vals = get_col_indx(columns)
+        column_vals = get_col_indx(columns) 
         data = pd.read_csv(file_path)
         data['primaryTitle'] = data['primaryTitle'].str.strip() 
         data = data.values.tolist()
 
         n = len(data)
-        arr = []
+        arr = [data[0]]
         count = 1
-        for i in range(n):
-            if i % memory_limitation == 0 and i!=0:
-                # sort arr then save
-                arr.append(data[i])
+        for i in range(1, n):
+            arr.append(data[i])
+            if len(arr) == 2000:
                 merge_sort(arr, column_vals)
-                df = pd.DataFrame(arr) # columns=column_names
-                filename = 'Final/Sorted_' + str(count)
+                df = pd.DataFrame(arr)
+                filename = 'Individual/Sorted_' + str(count)
                 df.to_csv(filename + '.csv', sep=',', index=False, header=False)
                 arr = []
                 count+=1
-            else:
-                arr.append(data[i])
 
         merge_sort(arr, column_vals)
-        df = pd.DataFrame(arr) # columns=column_names
-        # df.set_index('tconst', inplace=True)
-        filename = 'Final/Sorted_' + str(count)
-        df.to_csv(filename + '.csv', sep=',', index=False)
+        df = pd.DataFrame(arr)
+        filename = 'Individual/Sorted_' + str(count)
+        df.to_csv(filename + '.csv', sep=',', index=False, header=False)
         arr = []
 
+# return half of file in a list form
+def half_of_file(path, isLower):
+    data = pd.read_csv(path, header=None)
+    data = data.values.tolist()
+    if isLower:
+        return data[0:1000]
+    else:
+        return data[1000:2000]
+    
+def ith_of_file(path, start, end):
+    data = pd.read_csv(path, header=None)
+    data = data.values.tolist()
+    return data[start:end]
+
+# save list to csv
+def save_to_csv(arr, path):
+    df = pd.DataFrame(arr)
+    df.to_csv(path, sep=',', index=False, header=False)
+
 def Mystery_Function(file_path, memory_limitation, columns):
-    """
-    # file_path :  file_path for Individual Folder (datatype : String)
-    # memory_limitation : At each time how many records from the dataframe can be loaded (datatype : integer : 2000)
-    # columns : the columns on which dataset needs to be sorted (datatype : list of strings)
-    # **NOTE : In this Mystery_Function records are accessed from only the folder Individual.
+    column_vals = get_col_indx(columns)
 
-    #Store all the output files in Folder named "Final".
-    #The below Syntax will help you to store the sorted files :
-                # name_of_csv = "Final/Sorted_" + str(i + 1)
-                # sorted_df.reset_index(drop=True).to_csv(name_of_csv, index=False)
-    #Output csv files must be named in the format Sorted_1, Sorted_2,...., Sorted_93
-    # ***NOTE : Every output csv file must have 2000 sorted records except for the last ouput csv file which might have less
-                #than 2000 records.
-    """
+    # for i in range(1, 94):
+    #     src = file_path + "/Sorted_" + str(i) + ".csv"
+    #     dst = "Final/Sorted_" + str(i) + ".csv"
+    #     data = pd.read_csv(src, header=None)
+    #     data = data.values.tolist()
+    #     merge_sort(data, column_vals)
+    #     save_to_csv(data, dst)
 
-    # Need to Code
-    # Helps to Sort all the 1,84,265 rows with limitation.
-    pass
+    # 2 temp files, max, min, temp?
 
+    
+    idx_check = {}
+    for i in range(1,94):
+        idx_check[i] = 0
+
+    path = "Final/Sorted_1.csv"
+    arr = []
+
+    i = 1
+    while len(arr) != 2000:
+        print(len(arr))
+        min = ith_of_file(path, idx_check[1], idx_check[1]+1)
+        min = min[0]
+        min_idx = 1
+        for i in range(1,93):
+            if idx_check[i] > 1999: continue
+            path = "Final/Sorted_" + str(i) + ".csv"
+            cur = ith_of_file(path, idx_check[i], idx_check[i]+1)
+            cur = cur[0]
+            if greaterThan(min, cur, column_vals):                
+                min = cur
+                min_idx = i
+
+        arr.append(min)
+        idx_check[min_idx]+=1
+
+    save_to_csv(cur, "temp.csv") 
+    #     path = "Final/Sorted_" + str(i) + ".csv"
+    #     cur_list += ith_of_file(path, 0, 21)
+
+    # for i in range(1,94):
+    #    if len(cur_list) == 2000: break 
+
+
+Mystery_Function("Individual", 2000, ['tconst', 'startYear','runtimeMinutes' ,'primaryTitle'])
 
 #Enable only one Function each from data_chuncks and Mystery_Function at a time
 
 #Test Case 13
-data_chuncks('imdb_dataset.csv', ['startYear'], 2000)
+# data_chuncks('imdb_dataset.csv', ['startYear'], 2000)
 
 #Test Case 14
 #data_chuncks('imdb_dataset.csv', ['primaryTitle'], 2000)
@@ -128,3 +203,73 @@ data_chuncks('imdb_dataset.csv', ['startYear'], 2000)
 
 #Test Case 15
 #Mystery_Function(file_path="Individual", 2000, ['startYear','runtimeMinutes' ,'primaryTitle'])
+
+
+
+'''
+
+def Mystery_Function(file_path, memory_limitation, columns):
+    column_vals = get_col_indx(columns)
+
+    # for i in range(1, 94):
+    #     src = file_path + "/Sorted_" + str(i) + ".csv"
+    #     dst = "Final/Sorted_" + str(i) + ".csv"
+    #     data = pd.read_csv(src, header=None)
+    #     data = data.values.tolist()
+    #     merge_sort(data, column_vals)
+    #     save_to_csv(data, dst)
+
+    # 2 temp files, max, min, temp?
+    i = 1
+    for j in range(i+1, 94):
+        
+        path  = "Final/Sorted_" + str(i) + '.csv'
+        path2 = "Final/Sorted_" + str(j) + '.csv'
+        
+        if j != 93:
+            arr1, arr2 = half_of_file(path, True), half_of_file(path2, True)
+            target_idx = -1
+
+            for idx in range(1000):
+                if greaterThan(arr1[idx], arr2[idx], column_vals):
+                    target_idx = idx
+                    break
+            
+            arr1, arr2 = half_of_file(path, False), half_of_file(path2, False)
+            if target_idx == -1:
+                for idx in range(1000):
+                    if greaterThan(arr1[idx], arr2[idx], column_vals):
+                        target_idx = idx
+                        break
+
+            if target_idx == -1: continue
+
+            temp_arr = ith_of_file(path, 0, target_idx) + ith_of_file(path2, 0, 2000-target_idx) # min values
+            insertion_sort(temp_arr, column_vals)
+            save_to_csv(temp_arr, "temp.csv")
+
+            
+            temp_arr = ith_of_file(path, target_idx, 2000) + ith_of_file(path2, 2000-target_idx, 2000) # min values
+            insertion_sort(temp_arr, column_vals)
+            save_to_csv(temp_arr, path2)
+
+            # temp_arr = arr1 + arr2 # min values
+            # merge_sort(temp_arr, column_vals)
+            # save_to_csv(temp_arr, "temp.csv")
+
+            # temp_arr = half_of_file(path, True) + half_of_file(path2, True)
+            # merge_sort(temp_arr, column_vals)
+            # save_to_csv(temp_arr, path2)
+
+            shutil.copyfile("temp.csv", path) # copy temp into path
+        else:
+            temp_arr = ith_of_file(path, 0, target_idx) + ith_of_file(path2, 0, 265) 
+            merge_sort(temp_arr, column_vals)
+            save_to_csv(temp_arr, "temp.csv")
+
+            temp_arr = ith_of_file(path, 1735, 2000)
+            merge_sort(temp_arr, columns)
+            save_to_csv(temp_arr, path2)
+
+            shutil.copyfile("temp.csv", path) # copy temp into path
+'''
---
+++ algorithms_project1_package/Mystery_Sorting/test.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+
+def half_of_file(path, isLower):
+    data = pd.read_csv(path, header=None)
+    data = data.values.tolist()
+    if isLower:
+        return data[0:1000]
+    else:
+        return data[1000:2000]
+    
+file_name = "Individual/Sorted_"
+
+data = pd.read_csv("Individual/Sorted_92.csv", header=None)
+data = data.values.tolist()
+
+
+data2 = pd.read_csv("Individual/Sorted_93.csv", header=None)
+data2 = data2.values.tolist()
+# data = data[999: 2000]
+
+# data1 = data[0:1000]
+# data2 = data[1000:2000]
+
+# print(len(data1))
+# print(data1)
+
+
+temp_arr = data[0:1735] + data2[0:265]
+
+
+print(len(data[1735:2000]))
+print(len(data2[0:265]))
+print(len(temp_arr))
+
+
+
+
+'''
+for i to 93
+    for j to 93
+
+    
+1250
+
+
+1000
+
+
+2000
+265    
+    
+'''
+
